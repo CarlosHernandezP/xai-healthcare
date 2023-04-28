333e6e6709c0aad2cf971373294ea46a97785822
pachoning <cc.pachon@gmail.com>
2023-04-28 21:08:49 +0200

    Codify variables in a different way

--- analysis.R
+++ analysis.R
@@ -50,4 +50,20 @@ for (var in variables) {
   print("-----------")
 }
 
+# 04 Create new categories for some variables ----
+age_low <- c("01-04 years", "05-09 years", "10-14 years", "15-19 years", "20-24 years", "25-29 years", "30-34 years", "35-39 years", "40-44 years")
+age_med <- c("45-49 years", "50-54 years", "55-59 years", "60-64 years")
 
+primary_site_first <- c("C44.0-Skin of lip, NOS", "C44.1-Eyelid", "C44.2-External ear", "C44.3-Skin other/unspec parts of face", "C44.4-Skin of scalp and neck")
+primary_site_second <- c("C44.5-Skin of trunk")
+
+data_trans <- data_trans %>% 
+  mutate(
+    age_rec = case_when(age_cat %in% age_low ~ "age_low", age_cat %in% age_med ~ "age_intermediate", .default = "age_advanced"),
+    race_rec = case_when(race == "White" ~ "white", .default = "other"),
+    marital_status_rec = case_when(marital_status == "Married (including common law)"  ~ "married", .default = "other"),
+    primary_site_rec = case_when(primary_site %in% primary_site_first ~ "first", primary_site %in% primary_site_second ~ "second", "third"),
+  )
+
+
+unique(data_trans$marital_status)
